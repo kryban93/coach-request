acc0d4100721ac5cade94afdcb8bcbce168f90f5
krystian.banot <krystian.banot@altkomsoftware.pl>
2023-01-12 09:16:42 +0100

    initiated routing, added pugjs to repo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: null },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/coaches/:id",
+      component: null,
+      children: [{ path: "/contact", component: null }],
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    { path: "/register", component: null },
+    { path: "/request", component: null },
+    { path: "/:notFound(.*)", component: null },
+  ],
+});
 
-export default router
+export default router;
