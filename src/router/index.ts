7724cc159b1a232c9b9d7e9a6ba4a6f6584eb497
krystian.banot <krystian.banot@altkomsoftware.pl>
2023-01-12 09:42:26 +0100

    updated routes, added main app file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachDetail from "../views/coaches/coach-detail/coach-detail.vue";
+import CoachList from "../views/coaches/coaches-list/coaches-list.vue";
+import CoachRegistration from "../views/coaches/coach-register/coach-register.vue";
+import ContactCoach from "../views/requests/contact-coach/contact-coach.vue";
+import RequestReceived from "../views/requests/request-received/request-received.vue";
+import NotFound from "../views/not-found/not-found.vue";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachList },
     {
       path: "/coaches/:id",
-      component: null,
-      children: [{ path: "/contact", component: null }],
+      component: CoachDetail,
+      children: [{ path: "/contact", component: ContactCoach }],
     },
-    { path: "/register", component: null },
-    { path: "/request", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegistration },
+    { path: "/request", component: RequestReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
