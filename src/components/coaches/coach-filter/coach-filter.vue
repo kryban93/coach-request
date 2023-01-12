d627d0b9aac502d2e09d0fca1e834187828aff7d
krystian.banot <krystian.banot@altkomsoftware.pl>
2023-01-12 19:15:58 +0100

    added filtering

---
+++ src/components/coaches/coach-filter/coach-filter.vue
@@ -0,0 +1,34 @@
+<template lang="pug" src="./coach-filter.pug"></template>
+<style scoped src="./coach-filter.css"></style>
+
+<script lang="ts">
+import type { Filters } from "./types";
+
+export default {
+	emits: ["change-filter"],
+	data() {
+		return {
+			filters: {
+				frontend: true,
+				backend: true,
+				career: true,
+			} as Filters,
+		};
+	},
+	methods: {
+		setFilter(event: Event) {
+			const target = event.target as HTMLInputElement;
+			if (target) {
+				const inputId = target.id;
+				const isActive = target.checked;
+				const updatedFilters = {
+					...this.filters,
+					[inputId]: isActive,
+				};
+				this.filters = updatedFilters;
+				this.$emit("change-filter", updatedFilters);
+			}
+		},
+	},
+};
+</script>
--- src/views/coaches/coaches-list/coaches-list.vue
+++ src/views/coaches/coaches-list/coaches-list.vue
@@ -1,20 +1,45 @@
 <template lang="pug" src="./coaches-list.pug"></template>
 <style scoped src="./coaches-list..css"></style>
 <script lang="ts">
-import type { Coach } from "../../../store/modules/coaches/types";
-import CoachItem from "../../../components/coaches/coach-item/coach-item.vue";
+import type { Coach } from "@/store/modules/coaches/types";
+import CoachItem from "@/components/coaches/coach-item/coach-item.vue";
+import CoachFilter from "@/components/coaches/coach-filter/coach-filter.vue";
+import type { Filters } from "@/components/coaches/coach-filter/types";
 export default {
 	computed: {
 		filteredCoaches(): Coach[] {
-			return this.$store.getters["coaches/coaches"];
+			const coaches = this.$store.getters["coaches/coaches"];
+			return coaches.filter((coach: Coach) => {
+				if (this.activeFilters.frontend && coach.areas.includes("fe")) {
+					return true;
+				}
+				if (this.activeFilters.backend && coach.areas.includes("be")) {
+					return true;
+				}
+				if (this.activeFilters.career && coach.areas.includes("career")) {
+					return true;
+				}
+				return false;
+			});
 		},
 		hasCoaches(): boolean {
 			return this.$store.getters["coaches/hasCoaches"];
 		},
 	},
 	data() {
-		return {};
+		return {
+			activeFilters: {
+				frontend: true,
+				backend: true,
+				career: true,
+			},
+		};
 	},
-	components: { CoachItem },
+	components: { CoachItem, CoachFilter },
+	methods: {
+		setFilters(updatedFilters: Filters) {
+			this.activeFilters = updatedFilters;
+		},
+	},
 };
 </script>
